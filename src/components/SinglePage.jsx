77071a09c000d45b86c474e79fdc863f6f5d621d
Olimka228 <olimjonshsh@gmail.com>
2023-10-28 16:24:22 +0500

    five-commit

--- src/components/SinglePage.jsx
+++ src/components/SinglePage.jsx
@@ -9,7 +9,7 @@ export default function SinglePage() {
     let singleProduct = product.find((item) => item.id === id)
   return (
     <div className="main">
-        <Header/>
+        <Header />
         <div className="singleproduct">
           <img src={singleProduct.img} alt="singleproduct" />
           <h1>Mahsulot: {singleProduct.title}</h1>
@@ -17,7 +17,7 @@ export default function SinglePage() {
           <h6>O'zbekistonda ishlab chiqarilgan</h6>
           <button onClick={() => addToBasket(id)}>Корзина</button>
         </div>
-        <Footer/>
+        <Footer />
     </div>
   )
 }
