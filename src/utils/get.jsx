13bd685d5906d5fdf7067a9af426fefacbf51d50
Olimka228 <olimjonshsh@gmail.com>
2023-10-31 16:01:23 +0500

    twentyNine-commit

--- src/utils/get.jsx
+++ src/utils/get.jsx
@@ -1,9 +1,5 @@
 const getStore = (key) => {
     return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : [];
  }
-  
-const getUser = () => {
-    return localStorage.getItem
-}
 
 export default getStore;
